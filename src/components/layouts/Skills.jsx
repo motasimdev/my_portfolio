0d0902f75f568f3c4722b733dcf65f7d661a9b7e
motasimdev <themotasimdev@gmail.com>
2025-10-04 18:44:03 +0600

    react icons added

--- src/components/layouts/Skills.jsx
+++ src/components/layouts/Skills.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Container from '../Container'
+import Flex from '../Flex'
+import { GrHtml5 } from "react-icons/gr";
 
 const Skills = () => {
   return (
     <>
-    <div className="py-[60px]"></div>
+    <div className="py-[60px] bg-[#3B939D]">
+        <Container>
+            <div className="">
+                <h3 className='text-primary font-semibold text-4xl text-center pb-[40px]'>Skills</h3>
+                <Flex className={"flex-wrap gap-x-7 justify-between"}>
+                    <div className="bg-[#D9D9D9] py-4 px-16">
+                        <GrHtml5 className='text-[40px]'/>
+                    </div>
+                    <div className="bg-[#D9D9D9] py-4 px-16">
+                        <GrHtml5 className='text-[40px]'/>
+                    </div>
+                    <div className="bg-[#D9D9D9] py-4 px-16">
+                        <GrHtml5 className='text-[40px]'/>
+                    </div>
+                    <div className="bg-[#D9D9D9] py-4 px-16">
+                        <GrHtml5 className='text-[40px]'/>
+                    </div>
+                    <div className="bg-[#D9D9D9] py-4 px-16">
+                        <GrHtml5 className='text-[40px]'/>
+                    </div>
+                    <div className="bg-[#D9D9D9] py-4 px-16">
+                        <GrHtml5 className='text-[40px]'/>
+                    </div>
+                </Flex>
+            </div>
+        </Container>
+    </div>
     </>
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../layouts/Banner'
 import About from '../layouts/About'
+import Skills from '../layouts/Skills'
 
 const Home = () => {
   return (
     <>
     <Banner/>
     <About/>
+    <Skills/>
     </>
   )
 }
