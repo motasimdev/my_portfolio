12df0fc24335850dbf48b18862bf6fccbe49e148
motasimdev <themotasimdev@gmail.com>
2025-10-05 00:02:09 +0600

    service part start

---
+++ src/components/layouts/Service.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from '../Container'
+
+const Service = () => {
+  return (
+    <>
+    <section className='bg-primary h-[300px]'>
+        <Container>
+            <div className=""></div>
+        </Container>
+    </section>
+    </>
+  )
+}
+
+export default Service
