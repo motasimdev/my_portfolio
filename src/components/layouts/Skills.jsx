2a741e3925df373d8e34b6c4728436ef9ce8c542
motasimdev <themotasimdev@gmail.com>
2025-10-04 23:10:54 +0600

    skills part running

--- src/components/layouts/Skills.jsx
+++ src/components/layouts/Skills.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Container from "../Container";
 import Flex from "../Flex";
 import { GrHtml5 } from "react-icons/gr";
-import { SiCss3 } from "react-icons/si";
+import { TbBrandCss3 } from "react-icons/tb";
+import { TbBrandJavascript } from "react-icons/tb";
 
 const Skills = () => {
   return (
@@ -14,10 +15,34 @@ const Skills = () => {
               Skills
             </h3>
             <Flex className={"flex-wrap gap-x-7 justify-between"}>
-              <div className="bg-[#D9D9D9] py-2 px-18">
-                <Flex>
+              <div className="bg-[#D9D9D9] py-4 px-10">
+                <Flex className={"gap-x-3"}>
                   <GrHtml5 className="text-[40px]" />
-                  <p className="font-medium text-black">HTML</p>
+                  <p className="font-medium text-black text-2xl">HTML</p>
+                </Flex>
+              </div>
+              <div className="bg-[#D9D9D9] py-4 px-10">
+                <Flex className={"gap-x-3"}>
+                  <TbBrandCss3 className="text-[50px]" />
+                  <p className="font-medium text-black text-2xl">CSS</p>
+                </Flex>
+              </div>
+              <div className="bg-[#D9D9D9] py-4 px-10">
+                <Flex className={"gap-x-3"}>
+                  <TbBrandJavascript className="text-[50px]" />
+                  <p className="font-medium text-black text-2xl">Javascript</p>
+                </Flex>
+              </div>
+              <div className="bg-[#D9D9D9] py-4 px-10">
+                <Flex className={"gap-x-3"}>
+                  <GrHtml5 className="text-[40px]" />
+                  <p className="font-medium text-black text-2xl">HTML</p>
+                </Flex>
+              </div>
+              <div className="bg-[#D9D9D9] py-4 px-10">
+                <Flex className={"gap-x-3"}>
+                  <GrHtml5 className="text-[40px]" />
+                  <p className="font-medium text-black text-2xl">HTML</p>
                 </Flex>
               </div>
             </Flex>
