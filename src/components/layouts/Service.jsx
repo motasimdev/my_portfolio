af52c981a67c9ca385313f65b772039668dbbebc
motasimdev <themotasimdev@gmail.com>
2025-10-05 00:06:03 +0600

    service part running

--- src/components/layouts/Service.jsx
+++ src/components/layouts/Service.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import Container from '../Container'
+import React from "react";
+import Container from "../Container";
+import Flex from "../Flex";
 
 const Service = () => {
   return (
     <>
-    <section className='bg-primary h-[300px]'>
+      <section className="bg-primary h-[300px]">
         <Container>
-            <div className=""></div>
+          <div className="">
+            <h3 className="text-[#E0FFFE] font-semibold text-4xl text-center py-[40px]">
+             My Services
+            </h3>
+            <Flex>
+                
+            </Flex>
+          </div>
         </Container>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../layouts/Banner'
 import About from '../layouts/About'
 import Skills from '../layouts/Skills'
+import Service from '../layouts/Service'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
     <Banner/>
     <About/>
     <Skills/>
+    <Service/>
     </>
   )
 }
