153e6974a53264cbc4d4dda27614c79637ddf6d4
motasimdev <themotasimdev@gmail.com>
2025-10-04 23:02:03 +0600

    skills part running

--- src/components/layouts/Skills.jsx
+++ src/components/layouts/Skills.jsx
@@ -1,40 +1,31 @@
-import React from 'react'
-import Container from '../Container'
-import Flex from '../Flex'
+import React from "react";
+import Container from "../Container";
+import Flex from "../Flex";
 import { GrHtml5 } from "react-icons/gr";
+import { SiCss3 } from "react-icons/si";
 
 const Skills = () => {
   return (
     <>
-    <div className="py-[60px] bg-[#3B939D]">
+      <div className="py-[60px] bg-[#3B939D]">
         <Container>
-            <div className="">
-                <h3 className='text-primary font-semibold text-4xl text-center pb-[40px]'>Skills</h3>
-                <Flex className={"flex-wrap gap-x-7 justify-between"}>
-                    <div className="bg-[#D9D9D9] py-4 px-16">
-                        <GrHtml5 className='text-[40px]'/>
-                    </div>
-                    <div className="bg-[#D9D9D9] py-4 px-16">
-                        <GrHtml5 className='text-[40px]'/>
-                    </div>
-                    <div className="bg-[#D9D9D9] py-4 px-16">
-                        <GrHtml5 className='text-[40px]'/>
-                    </div>
-                    <div className="bg-[#D9D9D9] py-4 px-16">
-                        <GrHtml5 className='text-[40px]'/>
-                    </div>
-                    <div className="bg-[#D9D9D9] py-4 px-16">
-                        <GrHtml5 className='text-[40px]'/>
-                    </div>
-                    <div className="bg-[#D9D9D9] py-4 px-16">
-                        <GrHtml5 className='text-[40px]'/>
-                    </div>
+          <div className="">
+            <h3 className="text-primary font-semibold text-4xl text-center pb-[40px]">
+              Skills
+            </h3>
+            <Flex className={"flex-wrap gap-x-7 justify-between"}>
+              <div className="bg-[#D9D9D9] py-2 px-18">
+                <Flex>
+                  <GrHtml5 className="text-[40px]" />
+                  <p className="font-medium text-black">HTML</p>
                 </Flex>
-            </div>
+              </div>
+            </Flex>
+          </div>
         </Container>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
