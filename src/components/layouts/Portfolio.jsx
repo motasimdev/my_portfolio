1f60e44e6b9c42be68f6ab324a36ccb40760819e
motasimdev <themotasimdev@gmail.com>
2025-10-05 12:47:25 +0600

    portfolio part start

---
+++ src/components/layouts/Portfolio.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from '../Container'
+
+const Portfolio = () => {
+  return (
+    <>
+    <section>
+        <div className="">
+            <Container>
+                <div className=""></div>
+            </Container>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Portfolio
