1b1b65564512eefe95107da9c3d6b1962d476520
motasimdev <themotasimdev@gmail.com>
2025-10-05 11:50:43 +0600

    service part runnign

--- src/components/layouts/Service.jsx
+++ src/components/layouts/Service.jsx
@@ -6,27 +6,28 @@ import { LiaTvSolid } from "react-icons/lia";
 const Service = () => {
   return (
     <>
-      <section className=" h-[480px]">
-        <div className="bg-[linear-gradient(180deg,rgba(11,86,99,1)_0%,rgba(21,93,105,1)_36%,rgba(28,98,110,1)_45%,rgba(39,106,117,1)_54%,rgba(58,119,129,1)_62%,rgba(80,134,143,1)_67%,rgba(255,255,255,1)_100%)] h-[350px]">
-          <Container>
+      <section className=" h-[520px] bg-[linear-gradient(180deg,rgba(11,86,99,1)_0%,rgba(21,93,105,1)_36%,rgba(28,98,110,1)_45%,rgba(39,106,117,1)_54%,rgba(58,119,129,1)_62%,rgba(80,134,143,1)_67%,rgba(255,255,255,1)_100%)]">
+        <Container>
           <div className="">
             <h3 className="text-[#E0FFFE] font-semibold text-4xl text-center py-[40px]">
               My Services
             </h3>
             <Flex className={"justify-between"}>
-              <div className="w-[380px] h-[360px] border border-white/20 bg-black/15 backdrop-blur-[15px] p-5">
+              <div className=" w-[380px] h-[380px] p-5 border border-white/20 bg-black/15 backdrop-blur-[15px] ">
+                <LiaTvSolid className="m-auto text-6xl mt-3 text-white" />
                 <h3 className="text-center text-2xl text-[#E0FFFE] py-5">
-                  <LiaTvSolid className=""/>
                   Web Design
                 </h3>
-                <p className="text-[#E0FFFE] text-base text-justify">
-                 I create modern, responsive, and visually appealing website designs focused on user experience and clean layout. Using tools like Figma and Tailwind CSS, I ensure every design is unique, functional, and perfectly aligned with your brand.
+                <p className="text-white text-base text-justify leading-[32px]">
+                  I create modern, responsive, and visually appealing website
+                  designs focused on user experience and clean layout. Using
+                  tools like Figma and Tailwind CSS, I ensure every design is
+                  unique, functional, and perfectly aligned with your brand.
                 </p>
               </div>
             </Flex>
           </div>
         </Container>
-        </div>
       </section>
     </>
   );
