e84b883a834b7f2d54c5476930633b3c678299d0
motasimdev <themotasimdev@gmail.com>
2025-10-05 17:27:07 +0600

    contact running

--- src/components/layouts/Contact.jsx
+++ src/components/layouts/Contact.jsx
@@ -8,14 +8,17 @@ const Contact = () => {
       <section>
         <div className="py-[100px] bg-[#3B939D]">
           <Container>
-              <div className="bg-[radial-gradient(circle,rgba(11,86,99,1)_0%,rgba(23,102,114,1)_0%,rgba(31,113,124,1)_25%,rgba(38,122,132,1)_38%,rgba(43,128,138,1)_53%,rgba(48,134,144,1)_66%,rgba(52,139,149,1)_79%,rgba(59,147,157,1)_100%)] text-center py-6 mb-[60px]">
-                <h3 className="text-[#E0FFFE] font-semibold text-4xl text-center">
-                  Contact
-                </h3>
-              </div>
+            <div className="bg-[radial-gradient(circle,rgba(11,86,99,1)_0%,rgba(23,102,114,1)_0%,rgba(31,113,124,1)_25%,rgba(38,122,132,1)_38%,rgba(43,128,138,1)_53%,rgba(48,134,144,1)_66%,rgba(52,139,149,1)_79%,rgba(59,147,157,1)_100%)] text-center py-6 mb-[60px]">
+              <h3 className="text-[#E0FFFE] font-semibold text-4xl text-center">
+                Contact
+              </h3>
+            </div>
             <div className="bg-primary p-10">
               <Flex>
-                
+                <div className="w-[45%]">
+                    <input type="text" placeholder="your name" className="py-4 px-5 w-[100%] border border-none focus:outline-0 bg-[#3B939D]"/>
+                </div>
+                <div className="w-[45%]"></div>
               </Flex>
             </div>
           </Container>
