bb944634a79ee1700e59204f1bcefe0659bad6d6
motasimdev <themotasimdev@gmail.com>
2025-10-05 17:16:14 +0600

    contact part runnning

--- src/components/layouts/Contact.jsx
+++ src/components/layouts/Contact.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import Container from "../Container";
 
 const Contact = () => {
   return (
     <>
-    
+      <section>
+        <div className="py-[100px] bg-[#3B939D]">
+          <Container>
+            <div className="">
+              <div className="text-center py-6 mb-[60px] bg-[radial-gradient(circle,rgba(23,102,114,1)_0%,rgba(54,122,133,1)_19%,rgba(70,132,143,1)_32%,rgba(83,141,151,1)_47%,rgba(116,163,171,1)_68%,rgba(153,187,193,1)_100%,rgba(255,255,255,1)_100%)]">
+                <h3 className="text-[#E0FFFE] font-semibold text-4xl text-center">
+                  Portfolio
+                </h3>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
