27f0c1492afceffa3c18d01e742cc120bbbd5d67
motasimdev <themotasimdev@gmail.com>
2025-10-05 19:41:42 +0600

    footer part running

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from "react";
+import Container from "../Container";
+import {
+  AiOutlineFacebook,
+  AiOutlineLinkedin,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+import { IoLogoGithub } from "react-icons/io";
+import { RiTwitterXLine } from "react-icons/ri";
+import Flex from "../Flex";
 
 const Footer = () => {
   return (
     <>
-    <div className="bg-primary py-10">
-      <h3 className='text-white'>this is footer</h3>
-    </div>
+      <div className="bg-primary py-10">
+        <Container>
+          <div className="">
+            <h4 className="text-white font-medium text-lg">Connect with me</h4>
+            <Flex className={"gap-x-2"}>
+              <a href="">
+                <AiOutlineFacebook className="text-[#E0FFFE] text-2xl" />
+              </a>
+              <a href="">
+                <AiOutlineLinkedin className="text-[#E0FFFE] text-2xl" />
+              </a>
+              <a href="">
+                <RiTwitterXLine className="text-[#E0FFFE] text-2xl" />
+              </a>
+              <a href="">
+                <AiOutlineInstagram className="text-[#E0FFFE] text-2xl" />
+              </a>
+              <a href="">
+                <IoLogoGithub className="text-[#E0FFFE] text-2xl" />
+              </a>
+            </Flex>
+          </div>
+        </Container>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
