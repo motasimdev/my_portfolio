3a527ed9f4581b7a8511ac3742b517b72cfbf27b
motasimdev <themotasimdev@gmail.com>
2025-10-06 08:02:18 +0600

    add motion

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import Banner from '../layouts/Banner'
-import About from '../layouts/About'
-import Skills from '../layouts/Skills'
-import Service from '../layouts/Service'
-import Portfolio from '../layouts/Portfolio'
-import Contact from '../layouts/Contact'
+import React from "react";
+import Banner from "../layouts/Banner";
+import About from "../layouts/About";
+import Skills from "../layouts/Skills";
+import Service from "../layouts/Service";
+import Portfolio from "../layouts/Portfolio";
+import Contact from "../layouts/Contact";
+import { motion, useScroll } from "motion/react";
 
 const Home = () => {
+  const { scrollYProgress } = useScroll();
   return (
     <>
-    <Banner/>
-    <About/>
-    <Skills/>
-    <Service/>
-    <Portfolio/>
-    <Contact/>
+      <motion.div
+        className="h-2 bg-primary w-full origin-left fixed top-0 left-0 z-[100]"
+        style={{
+          scaleX: scrollYProgress,
+        }}
+      ></motion.div>
+      <Banner />
+      <About />
+      <Skills />
+      <Service />
+      <Portfolio />
+      <Contact />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
