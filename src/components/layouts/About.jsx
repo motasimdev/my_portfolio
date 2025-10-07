20b3ef18932f3aa6b57caf55ff07799aea2f00eb
motasimdev <themotasimdev@gmail.com>
2025-10-07 10:11:03 +0600

    about responsive almost done

--- src/components/layouts/About.jsx
+++ src/components/layouts/About.jsx
@@ -1,67 +1,83 @@
 import React from "react";
 import Container from "../Container";
 import Flex from "../Flex";
-import { motion } from "motion/react"
+import { easeIn, easeInOut, easeOut, motion } from "motion/react";
 
 const About = () => {
   return (
     <>
-      <section id="aboutMe" className="bg-primary py-[60px] scroll-mt-[15px] h-[700px] flex items-center">
+      <section
+        id="aboutMe"
+        className="bg-primary pt-[110px] lg:pt-[60px] pb-[0px] lg:pb-[60px] scroll-mt-[15px] h-[600px] lg:h-[700px] flex items-center"
+      >
         <Container>
-          <Flex className={"justify-between"}>
-            <div className="w-[50%] flex flex-col gap-y-[60px] relative">
-              <div className="bg-[#72C6CF] w-[400px] h-[90px] ml-auto"></div>
-              <div className="bg-[#72C6CF] w-[400px] h-[90px] ml-auto"></div>
-              
-              
-             
-               
-                <motion.img
+          <div className={"lg:flex lg:items-center justify-between"}>
+            <div className="lg:w-[50%] lg:flex lg:flex-col lg:gap-y-[60px] relative">
+              <div className="hidden lg:block bg-[#72C6CF] w-[100px] lg:w-[400px] h-[50px] lg:h-[90px] ml-auto"></div>
+              <div className="hidden lg:block bg-[#72C6CF] w-[100px] lg:w-[400px] h-[50px] lg:h-[90px] ml-auto"></div>
+
+              <motion.img
                 // ======animate=======
                 initial={{
-                x: 150,
-              }}
+                  x: 150,
+                }}
                 whileInView={{
-                  x : 0,
-                  y:0,
+                  x: 0,
+                  y: 0,
                 }}
                 viewport={{
-                  once:false,
-                  amount:0.2,
+                  once: false,
+                  amount: 0.2,
                 }}
                 transition={{
-                  duration:2,
+                  duration: 2,
                 }}
                 whileHover={{
-                  scale:1.04
+                  scale: 1.05,
+                  transition: {
+                    duration: 0.2,
+                    
+                  },
                 }}
                 // ========animate=====
                 src={
                   "https://res.cloudinary.com/dxihqxcjs/image/upload/v1759516688/portfolioPic2_mgpmbt.png"
                 }
                 alt={""}
-                className={"absolute top-[-75px] left-0 w-[390px]"}
+                className={
+                  " lg:block absolute top-[-120px] lg:top-[-75px] left-1/2 lg:left-0 -translate-x-1/2 lg:-translate-x-0 w-[190px] lg:w-[390px]"
+                }
               />
-              
             </div>
-            <div className="w-[50%] py-[50px] px-[40px] bg-[#72C6CF]">
-              <h3 className="text-primary font-semibold text-4xl text-center pb-[40px]">
+            <div className="m-auto w-[90%] lg:w-[50%] py-[30px] lg:py-[50px] px-[30px] lg:px-[40px] bg-[#72C6CF]">
+              <h3 className="text-primary font-semibold text-3xl lg:text-4xl text-center pb-[20px] lg:pb-[40px] relative z-10">
                 About me
               </h3>
-              <p className="text-white font-medium text-base text-justify leading-[27px]">
-               I am a passionate Front-End Web Developer specializing in HTML, CSS, Tailwind CSS, JavaScript, React.js, and Redux. I can convert Figma designs into responsive, pixel-perfect web templates and bring them to life using React.js. Experienced with jQuery plugins and focused on modern front-end development practices.
+              <p className="text-white font-medium text-sm lg:text-base text-justify leading-[22px] lg:leading-[27px]">
+                I am a passionate Front-End Web Developer specializing in HTML,
+                CSS, Tailwind CSS, JavaScript, React.js, and Redux. I can
+                convert Figma designs into responsive, pixel-perfect web
+                templates and bring them to life using React.js. Experienced
+                with jQuery plugins and focused on modern front-end development
+                practices.
               </p>
               <Flex className="justify-center gap-x-6 pt-9">
-                <button class="cursor-pointer font-semibold overflow-hidden relative   bg-black/15 backdrop-blur-[15px] border-[#72C6CF] group px-8 py-3">
-                      <span class="relative z-10 text-primary group-hover:text-white text-md duration-500">
-                         Download CV
-                      </span>
-                      <span class="absolute w-full h-full bg-primary -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
-                      <span class="absolute w-full h-full bg-primary -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
-                    </button>
+                <button class="hidden lg:block cursor-pointer font-semibold overflow-hidden relative bg-black/15 backdrop-blur-[15px] border-[#72C6CF] group px-4 lg:px-8 py-3 lg:py-3">
+                  <span class="relative z-10 text-primary group-hover:text-white text-md duration-500">
+                    Download CV
+                  </span>
+                  <span class="absolute w-full h-full bg-primary -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+                  <span class="absolute w-full h-full bg-primary -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+                </button>
+
+                {/* <button class="group flex h-fit w-fit flex-col items-center justify-center rounded-full bg-[#F1ddcf] px-[2em] py-[0.5em] shadow-[inset_0px_2px_4px_0px_#f9f1eb,inset_0px_-2px_4px_0px_#e8c8b0,0px_-2px_16px_0px_#e8c8b0,0px_2px_16px_0px_#f9f1eb] duration-200 hover:translate-y-[5%] active:translate-y-[7%] active:shadow-[inset_0px_-2px_4px_0px_#f9f1eb,inset_0px_2px_4px_0px_#e8c8b0,0px_2px_16px_0px_#e8c8b0,0px_2px_16px_0px_#f9f1eb]">
+                  <p class="font-nunito text-[1.5em] font-semibold text-[#d19466] duration-200 group-active:translate-y-[5%]">
+                    Button
+                  </p>
+                </button> */}
               </Flex>
             </div>
-          </Flex>
+          </div>
         </Container>
       </section>
     </>
