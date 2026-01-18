1581994c56dbea3baebae44d0f0b1d54336cfc71
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-18 23:26:05 +0600

    portfolio running

--- src/components/layouts/Portfolioo.jsx
+++ src/components/layouts/Portfolioo.jsx
@@ -41,7 +41,6 @@ const Portfolioo = () => {
                     </p>
                     <Flex className={"gap-x-5 mt-8"}>
                       <Link className="py-3 px-5 bg-red-300">link</Link>
-                      <Link className="py-3 px-5 bg-red-300">github</Link>
                     </Flex>
                   </div>
                 </div>
