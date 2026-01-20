66962d6fd4352eb85187a5a261c75026150686a2
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-21 00:18:30 +0600

    icon add

--- src/components/LinkIcon.jsx
+++ src/components/LinkIcon.jsx
@@ -4,21 +4,24 @@ import LINK_ICON from "../assets/icons/linkIcon.json";
 
 const LinkIcon = ({ size, href, text }) => {
   const playerRef = useRef(null);
+  const containerRef = useRef(null);
 
-  //   useEffect(() => {
-  //     playerRef.current?.playFromBeginning();
-  //   }, []);
-  const handleIcon = () => {
+  useEffect(() => {
     playerRef.current?.playFromBeginning();
-  };
+  }, []);
 
+  // desktop hover
+  const handleIcon = () => {
+    if (window.innerWidth >= 1024) {
+      playerRef.current?.playFromBeginning();
+    }
+  };
   return (
     <a
       href={href}
       className={`pl-6 pr-3 py-2 lg:text-primary text-[15px] text-shadow-lg lg:text-lg font-medium flex gap-x-2 shadow-lg items-center border border-white/20 bg-white/10 backdrop-blur-[px] cursor-pointer `}
       onMouseEnter={handleIcon}
       onFocus={handleIcon}
-
     >
       {text}
       <Player ref={playerRef} icon={LINK_ICON} size={size} />
