7b6b3d96cd9d564d05b5ce0b09a7eba8867865e0
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-21 23:58:12 +0600

    icon add setting

--- src/components/layouts/Portfolioo.jsx
+++ src/components/layouts/Portfolioo.jsx
@@ -24,36 +24,66 @@ const Portfolioo = () => {
               </div>
 
               <div className="px-3 lg:px-0">
-                {/* =========project 1======== */}
-                <div className="p-1.5 pb-4 md:p-2 border border-white/20 bg-white/10 backdrop-blur-sm md:flex justify-between shadow-[6px_11px_17px_0px_rgba(0,0,0,0.2)]">
-                  <div className="md:w-[44%] lg:w-[50%] overflow-hidden">
-                    <img
-                      src="https://res.cloudinary.com/dxihqxcjs/image/upload/f_auto,q_auto/f_auto,q_auto/v1768544464/gotrip_spaofy.jpg"
-                      alt="project"
-                      className="h-50 md:h-62.5 lg:h-100 w-full"
-                    />
+                  {/* =========project 1======== */}
+                  <div className="p-1.5 pb-4 md:p-2 border border-white/20 bg-white/10 backdrop-blur-sm md:flex justify-between shadow-[6px_11px_17px_0px_rgba(0,0,0,0.2)]">
+                    <div className="md:w-[44%] lg:w-[50%] overflow-hidden">
+                      <img
+                        src="https://res.cloudinary.com/dxihqxcjs/image/upload/f_auto,q_auto/f_auto,q_auto/v1768544464/gotrip_spaofy.jpg"
+                        alt="project"
+                        className="h-50 md:h-62.5 lg:h-100 w-full"
+                      />
+                    </div>
+                    <div className="md:w-[53%] lg:pt-8 lg:w-[45%] px-2 lg:px-0">
+                      <h4 className="text-xl md:text-2xl lg:text-3xl font-medium md:font-semibold text-[#E0FFFE] text-center md:text-left py-3 lg:py-5">
+                        Project Brief
+                      </h4>
+                      <p className="md:pr-14 text-white text-[13px] lg:text-base text-justify leading-6 lg:leading-8">
+                        It’s an E-commerce Web Application with product listing,
+                        add to cart, remove from cart, product search, responsive
+                        design, dynamic UI with React.js, state management with v
+                        Redux, and many more.
+                      </p>
+                      <Flex className={"justify-center md:justify-normal gap-x-5 mt-4 md:mt-9 lg:mt-15"}>
+                        <div className="">
+                          <LordIconSetup icon={LINK_ICON} text={"Live Link"}/>
+                        </div>
+                        <div className="">
+                          <LordIconSetup icon={GITHUB_ICON} text={"Github Source"}/>
+                        </div>
+                      </Flex>
+                    </div>
                   </div>
-                  <div className="md:w-[53%] lg:pt-8 lg:w-[45%] px-2 lg:px-0">
-                    <h4 className="text-xl md:text-2xl lg:text-3xl font-medium md:font-semibold text-[#E0FFFE] text-center md:text-left py-3 lg:py-5">
-                      Project Brief
-                    </h4>
-                    <p className="md:pr-14 text-white text-[13px] lg:text-base text-justify leading-6 lg:leading-8">
-                      It’s an E-commerce Web Application with product listing,
-                      add to cart, remove from cart, product search, responsive
-                      design, dynamic UI with React.js, state management with v
-                      Redux, and many more.
-                    </p>
-                    <Flex className={"justify-center md:justify-normal gap-x-5 mt-4 md:mt-9 lg:mt-15"}>
-                      <div className="">
-                        <LordIconSetup icon={LINK_ICON} text={"Live Link"}/>
-                      </div>
-                      <div className="">
-                        <LordIconSetup icon={GITHUB_ICON} text={"Github Source"}/>
-                      </div>
-                    </Flex>
+                  {/* =========project 1======== */}
+                  {/* =========project 1======== */}
+                  <div className="p-1.5 pb-4 md:p-2 border border-white/20 bg-white/10 backdrop-blur-sm md:flex justify-between shadow-[6px_11px_17px_0px_rgba(0,0,0,0.2)]">
+                    <div className="md:w-[44%] lg:w-[50%] overflow-hidden">
+                      <img
+                        src="https://res.cloudinary.com/dxihqxcjs/image/upload/f_auto,q_auto/f_auto,q_auto/v1768544464/gotrip_spaofy.jpg"
+                        alt="project"
+                        className="h-50 md:h-62.5 lg:h-100 w-full"
+                      />
+                    </div>
+                    <div className="md:w-[53%] lg:pt-8 lg:w-[45%] px-2 lg:px-0">
+                      <h4 className="text-xl md:text-2xl lg:text-3xl font-medium md:font-semibold text-[#E0FFFE] text-center md:text-left py-3 lg:py-5">
+                        Project Brief
+                      </h4>
+                      <p className="md:pr-14 text-white text-[13px] lg:text-base text-justify leading-6 lg:leading-8">
+                        It’s an E-commerce Web Application with product listing,
+                        add to cart, remove from cart, product search, responsive
+                        design, dynamic UI with React.js, state management with v
+                        Redux, and many more.
+                      </p>
+                      <Flex className={"justify-center md:justify-normal gap-x-5 mt-4 md:mt-9 lg:mt-15"}>
+                        <div className="">
+                          <LordIconSetup icon={LINK_ICON} text={"Live Link"}/>
+                        </div>
+                        <div className="">
+                          <LordIconSetup icon={GITHUB_ICON} text={"Github Source"}/>
+                        </div>
+                      </Flex>
+                    </div>
                   </div>
-                </div>
-                {/* =========project 1======== */}
+                  {/* =========project 1======== */}
               </div>
             </div>
           </Container>
