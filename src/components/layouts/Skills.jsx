33d20c9f97d9804d8622fd0aa1d5b55128356df5
MD Motasim Billah <themotasimdev@gmail.com>
2026-01-31 23:35:16 +0600

    changing

--- src/components/layouts/Skills.jsx
+++ src/components/layouts/Skills.jsx
@@ -28,7 +28,7 @@ const Skills = () => {
       >
         <Container>
           <div className="">
-            <div className="bg-[radial-gradient(circle,rgba(11,86,99,1)_0%,rgba(23,102,114,1)_0%,rgba(31,113,124,1)_25%,rgba(38,122,132,1)_38%,rgba(43,128,138,1)_53%,rgba(48,134,144,1)_66%,rgba(52,139,149,1)_79%,rgba(59,147,157,1)_100%)] text-center py-3 lg:py-6 mb-10 lg:mb-[100px]">
+            <div className="bg-[radial-gradient(circle,rgba(11,86,99,1)_0%,rgba(23,102,114,1)_0%,rgba(31,113,124,1)_25%,rgba(38,122,132,1)_38%,rgba(43,128,138,1)_53%,rgba(48,134,144,1)_66%,rgba(52,139,149,1)_79%,rgba(59,147,157,1)_100%)] text-center py-3 lg:py-6 mb-10 lg:mb-25">
               <h3 className="text-[#E0FFFE] font-semibold text-2xl lg:text-4xl text-center">
                 My Skills
               </h3>
