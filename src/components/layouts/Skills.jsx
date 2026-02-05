c299fc1d158309aa2c6ad921c1f13f80fe5be800
MD Motasim Billah <themotasimdev@gmail.com>
2026-02-06 01:21:30 +0600

    changing

--- src/components/layouts/Skills.jsx
+++ src/components/layouts/Skills.jsx
@@ -56,7 +56,7 @@ const Skills = () => {
                   </p>
                 </div>
 
-                <div className="bg-primary flex justify-center items-center h-[36px] lg:h-[50px] w-[110px] lg:w-[160px] cursor-pointer">
+                <div className="bg-primary flex justify-center items-center h-9 lg:h-12.5 w-[110px] lg:w-[160px] cursor-pointer">
                   <RiTailwindCssFill className="text-4 lg:text-[30px] text-[#E0FFFE]" />
                   <p className="font-medium text-[#E0FFFE] text-[13px] lg:text-md">
                     Tailwind Css
